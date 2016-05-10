a1114e1733dbb9f7d8c53fc349ed3508164fe191
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2016-05-10 08:12:00 +0200

    factored out hash map

--- src/interaction_map.cpp
+++ src/interaction_map.cpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <limits>
-#include <iostream>
+
+#include <boost/shared_ptr.hpp>
 
 #include <boost/range.hpp>
 #include <boost/range/irange.hpp>
@@ -32,6 +33,56 @@ fits snugly in L1 cache
 the only way to make this faster would be to actively reorder the input points, to exploit temporal coherency in the lexsort
 */
 
+
+class HashMap {
+	const int entries;	//number of entries in hashmap
+	int_2 keys;	        //voxel coordinates uniquely identifying a bucket
+	int_1 values;	    //bucket description, or where to look in pivot array
+
+	inline int get_hash(const int3& p) const
+	{
+		const int3 c = p*primes;
+		return (c[0]^c[1]^c[2]) & (entries-1);
+	}
+
+public:
+	HashMap(const int entries):
+	    entries(entries),
+		keys({entries, 3}),
+		values({entries})
+	{
+		//mark grid as unoccupied
+		fill(values, -1);
+	}
+
+	inline void write(const int3& key, const int value)
+	{
+		const auto _keys = keys.range<int3>();
+		int entry = get_hash(key);				//get entry initial guess
+		// find an empty entry
+		while (true)
+		{
+			if (values[entry] == -1) break;
+			entry = (entry+1) & (entries-1);					//circular increment
+		}
+		values[entry] = value;
+		_keys[entry] = key;
+	}
+	inline int read(const int3& key) const
+	{
+		const auto _keys  = keys.range<const int3>();
+		int entry = get_hash(key);			//hash guess
+
+		while (true)						//find the right entry
+		{
+			if ((_keys[entry] == key).all()) return values[entry];	//we found it; this should be the most common code path
+			if (values[entry] == -1) return -1;	    				//if we didnt find it yet, we never will
+			entry = (entry+1) & (entries-1);						//circular increment
+		}
+	}
+};
+
+
 class VertexGridHash {
 public:
     /*
@@ -40,43 +91,38 @@ public:
     and is chosen here with future GPU-porting in mind, as it maps well to parallel architectures
     */
 
-	float_2 position;   //vertex positions
-	const int vertices; //number of vertices
-	int_2 cell_id;		//the cell coordinates a vertex resides in
-	int_1 indices;		//index array mapping the vertices to lexographically sorted order
+	float_2 position; //vertex positions
+	const int vertices;     //number of vertices
+
+	const float lengthscale;//size of a virtual voxel, which equals the maximum interaction range of a vertex-vertex pair query
+	float3 pmin, pmax;	    //maximum extent of pointcloud; used to map coordinates to positive integers
+	int3 size;			    //number of virtual buckets in each direction; used to prevent out-of-bound lookup
+
+	int_2 cell_id;	//the cell coordinates a vertex resides in
+	int_1 indices;	//index array mapping the vertices to lexographically sorted order
 	int_1 pivots;		//boundaries between buckets in vertices as viewed under indices
-	int buckets;		//number of buckets
+	int buckets;		    //number of buckets
 
-	int entries;		//number of entries in hashmap
-	int_2 grid_cell_id;	//voxel coordinates uniquely identifying a bucket; key of the hashmap
-	int_1 grid_bucket;	//bucket description, or where to look in pivot array; value of the hashmap
+	boost::shared_ptr<HashMap> hashmap;
 	
-	float3 pmin, pmax;	//maximum extent of pointcloud; used to map coordinates to positive integers
-	int3 size;			//number of virtual buckets in each direction; used to prevent out-of-bound lookup
-	const float lengthscale;	//size of a virtual voxel, which equals the maximum interaction range of a vertex-vertex pair query
-
 
 	//interface methods
-	float_2 get_position(){return this->position;}
+	float_2 get_position() const {return this->position;}
 	void set_position(float_2 position){this->position = position;}
-	int_2 get_cell_id(){return this->cell_id;}
+	int_2 get_cell_id() const {return this->cell_id;}
 	void set_cell_id(int_2 cell_id){this->cell_id = cell_id;}
-	int_1 get_indices(){return this->indices;}
+	int_1 get_indices() const {return this->indices;}
 	void set_indices(int_1 indices){this->indices = indices;}
-	int_1 get_pivots(){return this->pivots;}
+	int_1 get_pivots() const {return this->pivots;}
 	void set_pivots(int_1 pivots){this->pivots = pivots;}
-//	int_1 get_grid(){return (this->grid);}
-//	void set_grid(int_1 grid){(this->grid) = grid;}
 
 
 	VertexGridHash(float_2 position, float lengthscale):
 		position(position),
-		vertices(position.shape()[0]),
+		vertices(position.size()),
 		lengthscale(lengthscale),
-		cell_id({vertices,3}),
+		cell_id({vertices, 3}),
 		indices({vertices}),
-		grid_cell_id({0,3}),
-		grid_bucket({0}),
 		pivots({vertices+1})
 	{
 		//build the datastructure
@@ -85,6 +131,28 @@ public:
 		pivoting();
 		writegrid();
 	}
+
+
+	//map a global coord into the grid local coords
+	inline float3 transform(const float3& v) const
+	{
+		return (v-pmin) / lengthscale;
+	}
+	inline int3 cell_from_local_position(const float3& v) const
+	{
+		return (v-0.5).cast<int>();	//defacto floor
+	}
+	inline int3 cell_from_position(const float3& v) const
+	{
+		return cell_from_local_position(transform(v));
+	}
+	//convert bucket index into cell coords
+	inline int3 cell_from_bucket(const int b) const
+	{
+		const auto _cell_id  = cell_id.range<const int3>();
+		return _cell_id[indices[pivots[b]]];
+	}
+
 
 
 	//determine extents of data
@@ -143,104 +211,38 @@ public:
 	//build the hashmap to quickly access buckets of vertices at arbitrary locations
 	void writegrid()
 	{
-		//calc number of entries in hash table. table should have twice the number of buckets, at mimimum. 
+		//calc number of entries in hashmap. hashmap should have twice the number of buckets, at mimimum.
 		//absolute size is proportional to the number of vertices in the dataset, not to the space they occupy
-		entries = 64; while (entries < buckets*2) entries <<= 1; 
+		int entries = 64;
+		while (entries < buckets*2) entries <<= 1;
 
-		//allocate entry arrays. could use an index too
-		grid_cell_id = int_2({entries,3});
-		grid_bucket  = int_1({entries});
+        hashmap = boost::shared_ptr<HashMap>(new HashMap(entries));
 
-		//mark grid as unoccupied
-		fill(grid_bucket , -1);
 		//write buckets to the hash map
 		for (const int b: boost::irange(0, buckets))
-			write_bucket(cell_from_bucket(b), b);
+			hashmap->write(cell_from_bucket(b), b);
 	}
 
 
-
-
-	//map a global coord into the grid local coords
-	inline float3 transform(const float3& v)
+	boost::integer_range<int> indices_from_bucket(const int b) const
 	{
-		return (v-pmin) / lengthscale;
-	}
-	inline int3 cell_from_local_position(const float3& v)
-	{
-		return (v-0.5).cast<int>();	//defacto floor
-	}
-	inline int3 cell_from_position(const float3& v)
-	{
-		return cell_from_local_position(transform(v));
-	}
-
-	//convert bucket index into cell coords
-	inline int3 cell_from_bucket(const int b)
-	{
-		const auto _cell_id  = cell_id.range<const int3>();
-		return _cell_id[indices[pivots[b]]];
-	}
-	inline int get_hash(const int3& p)
-	{
-		const int3 c = p*primes;
-		return (c[0]^c[1]^c[2]) & (entries-1);
-	}
-	inline void write_bucket(const int3& cell, const int b)
-	{
-		const auto _grid_cell_id  = grid_cell_id.range<int3>();
-		int entry = get_hash(cell);				//get entry initial guess
-		// find an empty bucket
-		while (true) 
-		{
-			if (grid_bucket[entry]==-1) break;
-			entry = (entry+1) & (entries-1);					//circular increment			
-		}
-
-		grid_bucket[entry]  = b;
-		_grid_cell_id[entry] = cell;		//could store index into cell_id instead?
-	}
-	inline int read_bucket(const int3& cell)
-	{
-		const auto _grid_cell_id  = grid_cell_id.range<const int3>();
-		int entry = get_hash(cell);			//hash guess
-
-		while (true)						//find the right entry
-		{
-			if ((_grid_cell_id[entry]==cell).all()) return grid_bucket[entry];	//we found it; this should be the most common code path
-			if (grid_bucket[entry]==-1) return -1;								//if we didnt find it yet, we never will
-			entry = (entry+1) & (entries-1);									//circular increment			
+		if (b==-1){
+			return boost::irange(0,0);			//create empty range
+		}else{
+			return boost::irange(pivots[b], pivots[b+1]);
 		}
 	}
-	boost::integer_range<int> indices_from_bucket(const int b)
-	{
-		if (b==-1)
-		{
-			return boost::irange(0,0);						//create empty range
-		}
-		else
-		{
-			const int begin = pivots[b];
-			const int end   = pivots[b+1];
-			//if (end<begin) throw my_exception("infnite loop detected...");
-
-			return boost::irange(begin, end);
-		}
-	}
-
-
 	//abstract away the iteration over a gridcell
-	boost::iterator_range<boost::permutation_iterator<int*, boost::integer_range<int>::iterator>> 
-		vertices_from_cell(const int3& cell)
+	auto vertices_from_cell(const int3& cell) const
 	{
-		const auto piv = indices_from_bucket(read_bucket(cell));
+		const auto piv = indices_from_bucket(hashmap->read(cell));
 		const auto ind = indices.range<int>();			
 		return boost::make_iterator_range(
 			boost::make_permutation_iterator( ind.begin(), piv.begin() ), 
 			boost::make_permutation_iterator( ind.end(),   piv.end()   ));
 	}
 	template <class F>
-	void for_each_vertex_in_cell(const int3& cell, const F& body)
+	void for_each_vertex_in_cell(const int3& cell, const F& body) const
 	{
 		//would it help here if we permuted the bucket indices to achieve lexographic iteration?
 		for (const int i: indices_from_bucket(read_bucket(cell)))
@@ -249,7 +251,7 @@ public:
 
 	//loop over each occupied cell in the grid
 	template <class F>
-	void for_each_cell(const F& body)
+	void for_each_cell(const F& body) const
 	{
 		//iterate over entries instead? only helps if entry stores full cell info
 		for (const int b: boost::irange(0,buckets))
@@ -257,7 +259,7 @@ public:
 	}
 	//loop over bounding box and apply body
 	template <class F>
-	void for_each_vertex_in_bounding_box(const float3& gmin, const float3& gmax, const F& body)
+	void for_each_vertex_in_bounding_box(const float3& gmin, const float3& gmax, const F& body) const
 	{
 		const auto _position = position.range<const float3>();
 		const auto in_box = [&](const int v)
@@ -285,7 +287,7 @@ public:
 	}
 	//for unit testing purposes
 	template <class F>
-	void for_each_vertex_in_bounding_box_naive(const float3& gmin, const float3& gmax, const F& body)
+	void for_each_vertex_in_bounding_box_naive(const float3& gmin, const float3& gmax, const F& body) const
 	{
 		const auto _position = position.range<const float3>();
 		const auto in_box = [&](const int v)
