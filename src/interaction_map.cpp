d62e0aa7b7eb44922c11cfa4b5c41acbeb823630
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2016-05-19 07:24:42 +0200

    add gridspec

--- src/interaction_map.cpp
+++ src/interaction_map.cpp
@@ -39,6 +39,80 @@ fits snugly in L1 cache
 the only way to make this faster would be to actively reorder the input points, to exploit temporal coherency in the lexsort
 */
 
+
+template<typename real_t, typename fixed_t, int NDim>
+class GridSpec {
+public:
+	typedef GridSpec<real_t, fixed_t, NDim> self_t;
+	typedef real_t                          real_t;     // expose as public
+	typedef int32_t                         index_t;       // 32 bit int is a fine size type; 4 billion points isnt very likely
+	typedef int64_t                         hash_t;
+
+	typedef Eigen::Array<real_t, 2, NDim>	box_t;
+	typedef Eigen::Array<real_t, 1, NDim>	vector_t;
+	typedef Eigen::Array<fixed_t, 1, NDim>	cell_t;
+
+	const real_t                 scale; // size of a virtual voxel
+
+	const box_t                  box;         // maximum extent of pointcloud; used to map coordinates to positive integers
+	const cell_t                 shape;       // number of virtual buckets in each direction; used to prevent out-of-bound lookup
+	const cell_t                 strides;     // for lex-ranking cells
+
+	GridSpec(ndarray<real_t, 2> position, real_t scale) :
+		scale(scale),
+		box(init_box(position)),
+		shape(init_shape()),
+		strides(init_strides()),
+		stencil(init_stencil(stencil))
+	{
+	}
+
+private:
+	//determine extents of data
+	auto init_box() const {
+		real_t inf = std::numeric_limits<real_t>::infinity();
+		box_t box;
+		box.row(0).fill(+inf);
+		box.row(1).fill(-inf);
+		for (vector_t p : position) {
+			box.row(0) = box.row(0).min(p);
+			box.row(1) = box.row(1).max(p);
+		}
+		return box;
+	}
+	// integer shape of the domain
+	cell_t init_shape() const {      // interestingly, using auto as return type fails spectacularly
+		return transform(box.row(1) - box.row(0)).cast<fixed_t>() + 1;	// use +0.5 before cast?
+	}
+	// find strides for efficient lexsort
+	auto init_strides() const {
+		//		boost::partial_sum(shape.cast<int>(), begin(strides), std::multiplies<int>());   // doesnt work somehow
+		cell_t strides;
+		strides(0) = 1;
+		for (auto i : irange(1, NDim))
+			strides(i) = strides(i - 1) * shape(i - 1);
+		return strides;
+	}
+
+public:
+	//map a global coord into the grid local coords
+	inline vector_t transform(const vector_t& v) const {
+		return (v - extents.row(0)) / scale;
+	}
+	inline cell_t cell_from_local_position(const vector_t& v) const {
+		return v.cast<fixed_t>();	// we want to round towards zero; surprised that we do not need a -0.5 for that..
+	}
+	inline cell_t cell_from_position(const vector_t& v) const {
+		return cell_from_local_position(transform(v));
+	}
+	inline fixed_t hash_from_cell(cell_t cell) const {
+		return (cell * strides).sum();
+	}
+
+};
+
+
+
 template<typename real_t, typename fixed_t, int NDim>
 class PointGrid {
 	/*
